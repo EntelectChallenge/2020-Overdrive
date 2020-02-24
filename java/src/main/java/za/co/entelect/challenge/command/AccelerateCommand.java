1c8ec9bd3c7cabd0e4d2979620479a083290e637
Jana Wessels <janawessels@outlook.com>
2020-02-24 21:37:50 +0200

    Fixed issue of running multiple rounds, updated direction command.

--- java/src/main/java/za/co/entelect/challenge/command/AccelerateCommand.java
+++ java/src/main/java/za/co/entelect/challenge/command/AccelerateCommand.java
@@ -1,12 +1,6 @@
 package za.co.entelect.challenge.command;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class AccelerateCommand implements Command {
-
-    public AccelerateCommand() {
-    }
 
     @Override
     public String render() {
