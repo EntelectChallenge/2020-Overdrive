12cbcc0d5be87939052dad74eee9796e29ba0dba
Jana Wessels <janawessels@outlook.com>
2020-02-29 12:32:02 +0200

    Fixed indexOutOfBounds issue

--- java/src/main/java/za/co/entelect/challenge/Bot.java
+++ java/src/main/java/za/co/entelect/challenge/Bot.java
@@ -32,11 +32,11 @@ public class Bot {
 
     public Command run() {
         List blocks = getBlocksInFront(myCar.position.lane, myCar.position.block, maxSpeed);
-
+        System.out.println(blocks);
         if (blocks.contains(Object.MUD)) {
             int i = random.nextInt(directionList.size());
             if (i == 0) {
-                return new DecelerateCommand();
+                return new AccelerateCommand();
             }
             return new ChangeLaneCommand(directionList.get(random.nextInt(directionList.size())));
         }
@@ -52,11 +52,14 @@ public class Bot {
                 .map(b -> b.position.lane)
                 .mapToInt(v -> v)
                 .max().orElse(0);
+
+        mapWidth = mapWidth - gameState.map[0].position.block;
+
         HashMap<Integer, Lane[]> map = new HashMap<Integer, Lane[]>();
 
         for (int lane = 1; lane <= mapHeight; lane++) {
-            Lane[] blocks = new Lane[mapWidth];
-            for (int block = 0; block < mapWidth; block++) {
+            Lane[] blocks = new Lane[mapWidth +2];
+            for (int block = 0; block <= mapWidth; block++) {
                 blocks[block] = (gameState.map[((lane - 1) * mapWidth) + block]);
             }
             map.put(lane, blocks);
@@ -72,10 +75,15 @@ public class Bot {
     private List getBlocksInFront(int lane, int block, int maxSpeed) {
         HashMap<Integer, Lane[]> map = getListMapStructure();
         List blocks = new ArrayList();
+        Integer startBlock = gameState.map[0].position.block;
 
         Lane[] laneList = map.get(lane);
-        for (int i = block - 1; i <= block + maxSpeed; i++) {
+        for (int i = block - startBlock; i <= block - startBlock + maxSpeed; i++) {
+            if (laneList[i] == null) {
+                break;
+            }
             blocks.add(laneList[i].object);
+
         }
         return blocks;
     }
