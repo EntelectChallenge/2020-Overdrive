a9900a9c62fe9b4b517e6d2488c9b5d38e870f02
Jana Wessels <janawessels@outlook.com>
2020-03-01 18:55:18 +0200

    Update states

--- java/src/main/java/za/co/entelect/challenge/enums/State.java
+++ java/src/main/java/za/co/entelect/challenge/enums/State.java
@@ -6,5 +6,15 @@ public enum State {
     @SerializedName("ACCELERATING")
     ACCELERATING,
     @SerializedName("READY")
-    READY
+    READY,
+    @SerializedName("NOTHING")
+    NOTHING,
+    @SerializedName("TURNING_RIGHT")
+    TURNING_RIGHT,
+    @SerializedName("TURNING_LEFT")
+    TURNING_LEFT,
+    @SerializedName("HIT_MUD")
+    HIT_MUD,
+    @SerializedName("HIT_OIL")
+    HIT_OIL
 }
