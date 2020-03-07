4ab8fba871b729f85e470cd71744f4ac4907dad1
Pierre Blaarkies Roux <pierre.roux6@gmail.com>
2020-03-07 17:16:50 +0200

    Change world map structure to a 2d array

--- src/main/scala/za/co/entelect/challenge/game/contracts/renderer/JsonRenderer.scala
+++ src/main/scala/za/co/entelect/challenge/game/contracts/renderer/JsonRenderer.scala
@@ -1,83 +1,88 @@
 package za.co.entelect.challenge.game.contracts.renderer
 
-import net.liftweb.json._
 import net.liftweb.json.JsonDSL._
-
-import za.co.entelect.challenge.game.contracts.game.GamePlayer
-import za.co.entelect.challenge.game.contracts.map.GameMap
-import za.co.entelect.challenge.game.contracts.map.CarGameMap
-import za.co.entelect.challenge.game.contracts.game.CarGamePlayer
+import net.liftweb.json._
 import za.co.entelect.challenge.game.contracts.Config.Config
+import za.co.entelect.challenge.game.contracts.game.CarGamePlayer
+import za.co.entelect.challenge.game.contracts.map.CarGameMap
 
 class JsonRenderer extends BaseMapRenderer {
 
-    override def renderFragment(gameMap: CarGameMap, gamePlayer: CarGamePlayer): String = {
-        val mapFragment = gameMap.getMapFragment(gamePlayer);
-        val mapFragmentPlayer = mapFragment.getPlayer();
-        val mapFragmentPlayerPosition = mapFragmentPlayer.getPosition();
-        val mapFragmentJsonStructure = 
-            ("currentRound" -> mapFragment.getCurrentRound()) ~
-            ("maxRounds" -> Config.MAX_ROUNDS) ~
-            ("player" ->
-                ("id" -> mapFragmentPlayer.getId()) ~
+  override def renderFragment(gameMap: CarGameMap, gamePlayer: CarGamePlayer): String = {
+    val mapFragment = gameMap.getMapFragment(gamePlayer)
+    val player = mapFragment.getPlayer()
+    val opponent = gameMap.getCarGamePlayers()
+      .find(p => p.getGamePlayerId() != player.getId()).get
+    val opponentBlock = mapFragment.getBlocks()
+      .find(b => b.occupiedByPlayerWithId == opponent.getGamePlayerId()).get
+    val mapFragmentJsonStructure =
+      ("currentRound" -> mapFragment.getCurrentRound()) ~
+        ("maxRounds" -> Config.MAX_ROUNDS) ~
+        ("player" ->
+          ("id" -> player.getId()) ~
+            ("position" ->
+              ("y" -> player.getPosition().getLane()) ~
+                ("x" -> player.getPosition().getBlockNumber())
+              ) ~
+            ("speed" -> player.getSpeed()) ~
+            ("state" -> player.getState()) ~
+            ("powerups" -> player.getPowerups().toList) ~
+            ("boosting" -> player.isBoosting()) ~
+            ("boostCounter" -> player.getBoostCounter())
+          ) ~
+        ("opponent" ->
+          ("id" -> opponent.getGamePlayerId()) ~
+            ("position" ->
+              ("y" -> opponentBlock.getPosition().getLane()) ~
+                ("x" -> opponentBlock.getPosition().getBlockNumber())
+              ) ~
+            ("speed" -> player.getSpeed())
+          ) ~
+        ("worldMap" -> mapFragment.getBlocks()
+          .groupBy { b => b.getPosition().getLane() }.toList
+          .map { kv =>
+            List(kv._2
+              .sortBy(b => b.getPosition().getBlockNumber())
+              .map { b =>
                 ("position" ->
-                    ("lane" -> mapFragmentPlayerPosition.getLane()) ~
-                    ("blockNumber" -> mapFragmentPlayerPosition.getBlockNumber())
-                ) ~
-                ("speed" -> mapFragmentPlayer.getSpeed()) ~
-                ("state" -> mapFragmentPlayer.getState()) ~
-                ("powerups" -> mapFragmentPlayer.getPowerups().toList) ~
-                ("boosting" -> mapFragmentPlayer.isBoosting()) ~
-                ("boostCounter" -> mapFragmentPlayer.getBoostCounter())
-            ) ~
-            ("lanes" -> 
-                mapFragment.getLanes().toList.map 
-                {
-                    l => 
-                    ("position" ->
-                        ("lane" -> l.getPosition().getLane()) ~
-                        ("blockNumber" -> l.getPosition().getBlockNumber())
-                    ) ~
-                    ("surfaceObject" -> l.getMapObject()) ~
-                    ("occupiedByPlayerWithId" -> l.getOccupiedByPlayerWithId())
-                }
-            );
-        
-        val jsonString = prettyRender(mapFragmentJsonStructure);
-        return jsonString;
-    }
+                  ("y" -> b.getPosition().getLane()) ~
+                    ("x" -> b.getPosition().getBlockNumber())
+                  ) ~
+                  ("surfaceObject" -> b.getMapObject()) ~
+                  ("occupiedByPlayerId" -> b.getOccupiedByPlayerWithId())
+              }.toSet)
+          })
 
-    override def renderVisualiserMap(gameMap: CarGameMap) : String = {
-        val globalMapJsonStructure = 
-            ("players" -> 
-                gameMap.getCarGamePlayers().toList.map 
-                {
-                    x => 
-                    ("id" -> x.getGamePlayerId()) ~
-                    ("position" ->
-                        ("lane" -> gameMap.getPlayerBlockPosition(x.getGamePlayerId()).getLane()) ~
-                        ("blockNumber" -> gameMap.getPlayerBlockPosition(x.getGamePlayerId()).getBlockNumber())
-                    ) ~
-                    ("speed" -> x.getSpeed()) ~
-                    ("state" -> x.getState()) ~
-                    ("powerups" -> x.getPowerups().toList) ~
-                    ("boosting" -> x.isBoosting()) ~
-                    ("boostCounter" -> x.getBoostCounter())
-                }
-            ) ~
-            ("blocks" -> 
-                gameMap.getBlocks().toList.map
-                {
-                    l => 
-                    ("position" ->
-                        ("lane" -> l.getPosition().getLane()) ~
-                        ("blockNumber" -> l.getPosition().getBlockNumber())
-                    ) ~
-                    ("surfaceObject" -> l.getMapObject()) ~
-                    ("occupiedByPlayerWithId" -> l.getOccupiedByPlayerWithId())
-                }
-            );
-        val jsonString = prettyRender(globalMapJsonStructure);
-        return jsonString;
-    }
+    prettyRender(mapFragmentJsonStructure)
+  }
+
+  override def renderVisualiserMap(gameMap: CarGameMap): String = {
+    val globalMapJsonStructure =
+      ("players" ->
+        gameMap.getCarGamePlayers().toList.map { p =>
+          ("id" -> p.getGamePlayerId()) ~
+            ("position" ->
+              ("lane" -> gameMap.getPlayerBlockPosition(p.getGamePlayerId()).getLane()) ~
+                ("blockNumber" -> gameMap.getPlayerBlockPosition(p.getGamePlayerId()).getBlockNumber())
+              ) ~
+            ("speed" -> p.getSpeed()) ~
+            ("state" -> p.getState()) ~
+            ("powerups" -> p.getPowerups().toList) ~
+            ("boosting" -> p.isBoosting()) ~
+            ("boostCounter" -> p.getBoostCounter())
+        }
+        ) ~
+        ("blocks" -> gameMap.getBlocks().toList
+          .map { b =>
+            ("position" ->
+              ("lane" -> b.getPosition().getLane()) ~
+                ("blockNumber" -> b.getPosition().getBlockNumber())
+              ) ~
+              ("surfaceObject" -> b.getMapObject()) ~
+              ("occupiedByPlayerWithId" -> b.getOccupiedByPlayerWithId())
+          }
+          )
+
+    prettyRender(globalMapJsonStructure)
+  }
 }
