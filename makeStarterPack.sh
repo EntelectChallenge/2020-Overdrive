e9bb94083b465a8de21b6fa09c2db65c84733175
Renette Ros <9079017+Mistborn94@users.noreply.github.com>
2020-04-09 14:55:54 +0200

    Setup build and starter pack publication
    * Build game engine, game runner and reference bot on pushes and pull requests
    * Publish starter pack on tag

--- makeStarterPack.sh
+++ makeStarterPack.sh
@@ -3,25 +3,21 @@
 mkdir starter-pack
 
 cd game-engine
-engineJarName=$(./gradlew fullJarName -q --console=plain)
-cp "build/libs/$engineJarName" ../starter-pack
+cp "target/scala-2.13/game-engine-jar-with-dependencies-0.1.jar" ../starter-pack/game-engine.jar
 cp default-config.json ../starter-pack/game-config.json
 cd ..
 
 cd game-runner
 cp makefile run.bat ../starter-pack
 cp starter-pack-runner-config.json ../starter-pack/game-runner-config.json
-cp target/*with-dependencies.jar ../starter-pack
+cp target/game-runner-jar-with-dependencies.jar ../starter-pack
 cd ..
 
 cp -r reference-bot starter-pack/
 cp -r starter-bots starter-pack/
 
 cd starter-pack
-sed -i "s/game-engine\.jar/.\/$engineJarName/g" game-runner-config.json
 sed -i "s/target/./g" run.bat makefile
 cd ..
-
-zip -r starter-pack.zip starter-pack/*
 
 
