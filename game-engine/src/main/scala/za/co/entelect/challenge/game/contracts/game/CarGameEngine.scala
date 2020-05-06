5e5283ad264780ee78812cda7f140fedfbbe9fa8
Kyle Mc Allister <kyle.mcallister@entelect.co.za>
2020-05-06 12:51:41 +0200

    Stop game once max rounds reached

--- game-engine/src/main/scala/za/co/entelect/challenge/game/contracts/game/CarGameEngine.scala
+++ game-engine/src/main/scala/za/co/entelect/challenge/game/contracts/game/CarGameEngine.scala
@@ -1,12 +1,13 @@
 package za.co.entelect.challenge.game.contracts.game
 
+import za.co.entelect.challenge.game.contracts.Config.Config
 import za.co.entelect.challenge.game.contracts.map.GameMap
 
 class CarGameEngine extends GameEngine {
 
     override def isGameComplete(gameMap: GameMap): Boolean = {
         val winner = gameMap.getWinningPlayer();
-        val gameIsComplete = winner != null;
+        val gameIsComplete = (winner != null) || (gameMap.getCurrentRound() > Config.MAX_ROUNDS);
         return gameIsComplete;
     }
 }
