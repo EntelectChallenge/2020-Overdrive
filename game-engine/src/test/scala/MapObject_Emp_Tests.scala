c52354961f526350b7127a987e383e46d5ad86d3
Jana-Wessels <janawessels@outlook.com>
2020-07-06 10:35:45 +0200

    Fix EMP pickup bug

---
+++ game-engine/src/test/scala/MapObject_Emp_Tests.scala
@@ -0,0 +1,41 @@
+import org.scalatest.FunSuite
+import test.TestHelper
+import za.co.entelect.challenge.game.contracts.Config.Config
+import za.co.entelect.challenge.game.contracts.command.RawCommand
+import za.co.entelect.challenge.game.contracts.commands.CommandFactory
+import za.co.entelect.challenge.game.contracts.game.CarGamePlayer
+
+class MapObject_Emp_Tests extends FunSuite{
+  private val commandText = "NOTHING"
+  private var commandFactory: CommandFactory = null
+  private var nothingCommand: RawCommand = null
+
+  def initialise() = {
+    Config.loadDefault()
+    commandFactory = new CommandFactory
+    nothingCommand = commandFactory.makeCommand(commandText)
+    nothingCommand.setCommand(commandText)
+  }
+
+  test("Given players during race when player hits EMP then player gains 1 EMP powerup") {
+    initialise()
+    val gameMap = TestHelper.initialiseGameWithMapObjectAt(1, 3, Config.EMP_MAP_OBJECT)
+    val testGamePlayer1 = TestHelper.getTestGamePlayer1()
+
+    val testCarGamePlayer = testGamePlayer1.asInstanceOf[CarGamePlayer]
+    TestHelper.processRound(gameMap, nothingCommand, nothingCommand)
+
+    assert(testCarGamePlayer.getPowerups().count(x => x == Config.EMP_POWERUP_ITEM) == 1)
+  }
+
+  test("Given players during race when player hits boost twice then player gains 2 EMP powerups") {
+    initialise()
+    val gameMap = TestHelper.initialiseGameWithMultipleSameMapObjectsAt(1, Array(3,4), Config.EMP_MAP_OBJECT)
+    val testGamePlayer1 = TestHelper.getTestGamePlayer1()
+
+    val testCarGamePlayer = testGamePlayer1.asInstanceOf[CarGamePlayer]
+    TestHelper.processRound(gameMap, nothingCommand, nothingCommand)
+
+    assert(testCarGamePlayer.getPowerups().count(x => x == Config.EMP_POWERUP_ITEM) == 2)
+  }
+}
