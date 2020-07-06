c2c9766fcfe39e7d1b0360978fade553c770549e
Kyle Mc Allister <kyle.mcallister@entelect.co.za>
2020-07-06 23:38:46 +0200

    Update emp

--- game-engine/src/main/scala/za/co/entelect/challenge/game/contracts/game/CarMapGenerator.scala
+++ game-engine/src/main/scala/za/co/entelect/challenge/game/contracts/game/CarMapGenerator.scala
@@ -69,7 +69,7 @@ class CarMapGenerator(seed: Int) extends GameMapGenerator {
         mapObjects.put(mapObjects.lastKey + ((Config.WALL_GENERATION_PERCENTAGE * 0.01) + 0.01) * 0.405, Config.WALL_MAP_OBJECT)
         mapObjects.put(mapObjects.lastKey + ((Config.LIZARD_GENERATION_PERCENTAGE * 0.01) + 0.01) * 0.203, Config.LIZARD_MAP_OBJECT)
         mapObjects.put(mapObjects.lastKey + ((Config.TWEET_GENERATION_PERCENTAGE * 0.01) + 0.01) * 0.22 , Config.TWEET_MAP_OBJECT)
-        mapObjects.put(mapObjects.lastKey + ((Config.EMP_GENERATION_PERCENTAGE * 0.01) + 0.01) * 0.21, Config.EMP_MAP_OBJECT)
+        mapObjects.put(mapObjects.lastKey + ((Config.EMP_GENERATION_PERCENTAGE * 0.01) + 0.01) * 0.24, Config.EMP_MAP_OBJECT)
         mapObjects.put(1.0, Config.EMPTY_MAP_OBJECT)
 
         //generate empty map
