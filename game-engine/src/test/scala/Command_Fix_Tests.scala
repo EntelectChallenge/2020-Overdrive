b98595005d73e7e228a0d5f5eb1d536d7662bf36
Edward John Steere <edward.steere@gmail.com>
2020-07-14 14:51:22 +0200

    Add a failing test for #106

--- game-engine/src/test/scala/Command_Fix_Tests.scala
+++ game-engine/src/test/scala/Command_Fix_Tests.scala
@@ -10,12 +10,16 @@ class Command_Fix_Tests extends FunSuite{
   private val commandText = "FIX"
   private var commandFactory: CommandFactory = null
   private var fixCommand: RawCommand = null
+  private val boostCommandText = "USE_BOOST"
+  private var boostCommand: RawCommand = null
 
   def initialise() = {
     Config.loadDefault()
     commandFactory = new CommandFactory
     fixCommand = commandFactory.makeCommand(commandText)
     fixCommand.setCommand(commandText)
+    boostCommand = commandFactory.makeCommand(boostCommandText)
+    boostCommand.setCommand(boostCommandText)
   }
 
   test("Given player with two damage uses fix command then has zero damage") {
@@ -24,7 +28,7 @@ class Command_Fix_Tests extends FunSuite{
     val testGamePlayer1 = TestHelper.getTestGamePlayer1()
     val testCarGamePlayer = testGamePlayer1.asInstanceOf[CarGamePlayer]
 
-    testCarGamePlayer.setDamage(2);
+    testCarGamePlayer.setDamage(2)
 
     TestHelper.processRound(gameMap, fixCommand, fixCommand)
 
@@ -34,13 +38,31 @@ class Command_Fix_Tests extends FunSuite{
     assert(testCarGamePlayer.getDamage == 0)
   }
 
+  test("Given player with two damage uses fix command then has unlimited max speed") {
+    initialise()
+    val gameMap = TestHelper.initialiseGameWithNoMapObjects()
+    val testGamePlayer1 = TestHelper.getTestGamePlayer1()
+    val testCarGamePlayer = testGamePlayer1.asInstanceOf[CarGamePlayer]
+    val initialSpeed = testCarGamePlayer.getSpeed
+
+    testCarGamePlayer.pickupBoost()
+    testCarGamePlayer.setDamage(2)
+    testCarGamePlayer.reduceMaxAllowableSpeed()
+
+    TestHelper.processRound(gameMap, fixCommand, fixCommand)
+
+    TestHelper.processRound(gameMap, boostCommand, boostCommand)
+
+    assert(testCarGamePlayer.speed == Config.BOOST_SPEED)
+  }
+
   test("Given player with speed uses fix command then does not move that round") {
     initialise()
     val gameMap = TestHelper.initialiseGameWithNoMapObjects()
     val testGamePlayer1 = TestHelper.getTestGamePlayer1()
     val testCarGamePlayer = testGamePlayer1.asInstanceOf[CarGamePlayer]
 
-    testCarGamePlayer.setDamage(2);
+    testCarGamePlayer.setDamage(2)
 
     TestHelper.processRound(gameMap, fixCommand, fixCommand)
 
