2fd8110575f3ed94913b786f2640dbcefc60b414
Kyle Mc Allister <kyle.mcallister@entelect.co.za>
2020-07-26 17:49:03 +0200

    Make sure player does not return to 15 speed when boost expires when player has no damage

--- game-engine/src/main/scala/za/co/entelect/challenge/game/contracts/game/CarGamePlayer.scala
+++ game-engine/src/main/scala/za/co/entelect/challenge/game/contracts/game/CarGamePlayer.scala
@@ -276,7 +276,7 @@ class CarGamePlayer(health: Int, var score: Int, gamePlayerId: Int, var speed: I
         val boostOver = boostCounter == 0
         if (boostOver) {
             boosting = false
-            speed = maxSpeedState
+            speed = Math.min(maxSpeedState, Config.MAXIMUM_SPEED)
             setState(Config.NOTHING_PLAYER_STATE)
         }
     }
